4a0a4bd76196031a1c05859c227961728236b6a3
Motunrayo <ajirotutu4real@gmail.com>
2024-05-20 10:58:19 +0100

    Take care of exception it basic_auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -55,7 +55,10 @@ class BasicAuth(Auth):
         """
         if type(user_email) != str or type(user_pwd) != str:
             return None
-        user = User.search({'email': user_email})
+        try:
+            user = User.search({'email': user_email})
+        except Exception:
+            return None
         if len(user) == 0:
             return None
         if user[0].is_valid_password(user_pwd):
