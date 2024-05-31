e01b84c3ea14b0870499de2d64a57fdf3ee0ff32
Motunrayo <ajirotutu4real@gmail.com>
2024-05-31 19:24:00 +0100

    Added more task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Flask App
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -37,6 +37,52 @@ def login():
         return res
     else:
         abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ Login Session """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
+
+
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """ Profile """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email})
+    else:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def reset_pwd():
+    """ Reset Password """
+    email = request.form.get("email")
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token})
+    except ValueError:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_pwd():
+    """ - """
+    email, token = request.form.get("email"), request.form.get("reset_token")
+    new_pwd = request.form.get("new_password")
+    try:
+        AUTH.update_password(token, new_pwd)
+        return jsonify({"email": email, "message": "Password updated"})
+    except ValueError:
+        abort(403)
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,5 +73,27 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """ Destroy Session
         """
-        self._db.update_user(user_id, session_id="")
+        self._db.update_user(user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Get Reset Password token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, session_id=reset_token)
+            return reset_token
+        except NoResultFound:
+            return ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Update Password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_pwd = _hash_password(password)
+            self._db.update_user(user.id, hash_password=hashed_pwd,
+                                 reset_token=None)
+        except NoResultFound:
+            return ValueError
