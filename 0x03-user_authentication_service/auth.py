fd914e86f407d688a071f59a4d82b09082d373d9
Motunrayo <ajirotutu4real@gmail.com>
2024-05-31 18:03:13 +0100

    Added task 9 & 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,12 +7,19 @@ from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 
 from user import User
+import uuid
 
 
 def _hash_password(password: str) -> str:
     """ Hash Password
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """ Generate UUID
+    """
+    return str(uuid.uuid4())
 
 
 class Auth:
@@ -42,3 +49,15 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
+        
+    def create_session(self, email: str) -> str:
+        """ Create a new Session
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
+
